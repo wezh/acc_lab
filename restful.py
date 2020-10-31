67b1f07c7ceeb785a15dff068b5b9ad28e893d83
wezh <mrwhuwh@gmail.com>
2020-10-31 15:08:01 +0000

    finish

--- restful.py
+++ restful.py
@@ -3,6 +3,8 @@ import sys
 import os, datetime
 from run_celery import tweets_count
 from celery.result import AsyncResult
+from celery.result import ResultSet
+from celery.result import GroupResult
 
 app = Flask(__name__)
 
@@ -22,18 +24,20 @@ pronoun_all = {'Han': 0,
 @app.route('/', methods=['GET'])
 def hello_world():
     return 'Hello World!'
+
 results = []
 result_status = []
+
 @app.route('/process', methods=['GET'])
 def dealing_tweets():
+    rs = ResultSet([])
     for tw_file in filelist:
         result = tweets_count.delay(tw_file)
         # result = result.get()
-        #print(result.get())
-        #res = AsyncResult(result.id)
-        print(result)
-        results.append(result)
-        result_status.append(result.status)
+        # print(result.get())
+        # res = AsyncResult(result.id)
+        rs.add(result)
+        # result_status.append(result.status)
         # pronoun_all['Han'] += result['Han']
         # pronoun_all['Hon'] += result['Hon']
         # pronoun_all['Den'] += result['Den']
@@ -41,6 +45,11 @@ def dealing_tweets():
         # pronoun_all['Denna'] += result['Denna']
         # pronoun_all['Denne'] += result['Denne']
         # pronoun_all['Hen'] += result['Hen']
+
+    for item in rs:
+        i = item.get()
+        results.append(i)
+    return 'Processing the result'
 
 @app.route('/result', methods=['GET'])
 def get_result():
@@ -52,6 +61,6 @@ def get_result():
         pronoun_all['Denna'] += r['Denna']
         pronoun_all['Denne'] += r['Denne']
         pronoun_all['Hen'] += r['Hen']
-    return pronoun_all['Han']
+    return "Han: " + str(pronoun_all['Han']) + '\n' + 'Hon: ' +str(pronoun_all['Hon']) + '\n' + 'Den: ' + str(pronoun_all['Den']) + '\n' + 'Det: ' +str(pronoun_all['Det']) + '\n' + 'Denna: ' +str(pronoun_all['Denna']) + '\n' + 'Denne: ' +str(pronoun_all['Denne']) + '\n' + "Hen: " + str(pronoun_all['Hen'])
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
